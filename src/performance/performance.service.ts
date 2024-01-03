05b54f8bb37f8c64e6ff8f506679dc64a643f975
Luizz Lee <leehanvit511@gmail.com>
2024-01-03 23:22:22 +0900

    feat[perfomance]: 예약 서비스 수정

--- src/performance/performance.service.ts
+++ src/performance/performance.service.ts
@@ -7,37 +7,37 @@ export class PerformanceService {
   constructor(private prisma: PrismaService) {}
 
   async getAllPerformance() {
-      const performances = this.prisma.performance.findMany({});
-      return performances;
+    const performances = this.prisma.performance.findMany({});
+    return performances;
   }
 
   async registerPerformance(dto: PerformanceDto, user_id) {
-      return this.prisma.performance.create({
-        data: {
-          performance_name: dto.performance_name,
-          price: dto.price,
-          venue: dto.venue,
-          content: dto.content,
-          user_id: user_id,
-        },
-      });
+    return this.prisma.performance.create({
+      data: {
+        performance_name: dto.performance_name,
+        price: dto.price,
+        venue: dto.venue,
+        content: dto.content,
+        user_id: user_id,
+      },
+    });
   }
 
   async getPerformancesByKeyword(dto: string) {
-      const performances = this.prisma.performance.findMany({
-        where: {
-          performance_name: {
-            contains: `${dto}`,
-          },
+    const performances = this.prisma.performance.findMany({
+      where: {
+        performance_name: {
+          contains: `${dto}`,
         },
-      });
-      return performances;
+      },
+    });
+    return performances;
   }
 
   async getPerformance(performance_id: number) {
     const performance = this.prisma.performance.findFirst({
-      where: { performance_id: performance_id }
+      where: { performance_id: performance_id },
     });
     return performance;
-}
+  }
 }
